ffaf4089940b6e6c1e2ac105736fa776f9e5aeb7
Duc <mymac2020.1@gmail.com>
2022-12-10 09:07:01 -0500

    added prefinal tests + modified main

--- main.cpp
+++ main.cpp
@@ -6,17 +6,20 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-//    w.show();
-//    return a.exec();
 
 
-    w.testPower();
-    w.testBattery();
-    w.testSessionSelection();
-    w.testConnection();
-    w.testIntensitySelection();
-    w.testRecordSession();
-    w.testReplaySession();
-    w.testSelectUser();
-        return 0;
+//  COMMENT THE FOLLOWING 2 LINES TO RUN TESTS
+    w.show();
+    return a.exec();
+
+////  UNCOMMENT BELOW FOR TESTS
+//    w.testPower();
+//    w.testBattery();
+//    w.testSessionSelection();
+//    w.testConnection();
+//    w.testIntensitySelection();
+//    w.testRecordSession();
+//    w.testReplaySession();
+//    w.testSelectUser();
+//        return 0;
 }
